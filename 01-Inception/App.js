ce0520ed89a5b270c37e8306dd7d27935cecd5d9
hrid1 <hridoycse1@gmail.com>
2024-07-03 13:58:02 +0600

    Start 01.Inception

---
+++ 01-Inception/App.js
@@ -0,0 +1,9 @@
+// use React
+
+alert("hi")
+
+const heading = React.createElement("h2", {}, "This is React 2.0");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+
